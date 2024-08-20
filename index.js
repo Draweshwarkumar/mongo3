902b217560cce6759f438d8c67d778b061ca769d
Draweshwarkumar <shivamjha22948@gmail.com>
2024-08-20 21:14:01 +0530

    sending data to databases

--- index.js
+++ index.js
@@ -3,9 +3,12 @@ const app = express();
 const mongoose = require('mongoose');
 const path = require("path");
 const chat = require("./models/chat.js");
+const exp = require("constants");
 
 app.set("views",path.join(__dirname,"views"));
 app.set("view engine","ejs");
+app.use(express.static(path.join(__dirname,"public")));
+app.use(express.urlencoded({extended: true}));
 
 main().then((res) =>{
     console.log("connection is successful");
@@ -29,13 +32,43 @@ chat1.save().then((res) =>{
 
 app.get("/chats", async(req,res) =>{
     let chats = await chat.find();
-    console.log(chats);
+    // console.log(chats);
     res.render("index.ejs",{chats});
-})
+});
+
+app.get("/chats/new",(req,res) =>{
+    res.render("new.ejs");
+});
+
+app.post("/chats",(req,res) =>{
+    let {from, to,msg} = req.body;
+    let newChat = new chat({
+        from: from,
+        to: to,
+        msg: msg,
+        created_at: new Date()
+    });
+    newChat
+    .save()
+    .then((res) =>{
+        console.log("chat was saved");
+    })
+    .catch((err) =>{
+        console.log(err);
+    })
+    res.redirect("/chats")
+});
+
+//Edit Route
+app.get("/chats/:id/edit", async (req, res) => {
+    let { id } = req.params;
+    let chat = await chat.findById(id); // Use 'Chat' here for consistency
+    res.render("edit.ejs", {chat});
+});
 
 app.get("/",(req,res) =>{
-    res.send("app is working");
-})
+   res.send("working");
+});
 
 app.listen(8080,()=>{
     console.log("server is listening on port 8080");
